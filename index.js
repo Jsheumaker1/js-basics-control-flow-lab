e9e6ed8b3c894387c48be3c3fd0b13ab15dda840
Jsheumaker1 <Jsheumaker1@gmail.com>
2021-04-27 14:49:52 -0500

    commit tree 21cc04630171610df8e4925c34103410a50ce39e

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
+  if (someNumber <= 400)
+    return `This one is on me!`
+
+  else if (someNumber > 2000 && someNumber <= 2500)
+    return `I will gladly take your thirty bucks.`
+
+  else if (someNumber > 2500)
+    return `No can do.`
+  }
+
+function ternaryCheckCity(town){
+  // Write your code here!
+  return  (town == "NYC" ? `Ok, sounds good.` : `No go.`);
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (tip){ 
+    case 'generous': return "Thank you so much."
+    case 'not as generous' : return "Thank you.";
+      break;
+      default : return "Bye."
+  }
 }
